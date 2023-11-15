1b9f39b8cb000ef2418fed1fa46cd491f46131e2
Alex Sanders <alex@sndrs.dev>
2023-11-15 09:22:33 +0000

    remove uneeded error flags

--- dotcom-rendering/scripts/webpack/webpack.config.server.js
+++ dotcom-rendering/scripts/webpack/webpack.config.server.js
@@ -38,7 +38,6 @@ module.exports = ({ sessionId }) => ({
 		runtimeChunk: false,
 	},
 	externals: [
-		// @ts-expect-error - webpack-node-externals types are incorrect
 		// https://github.com/liady/webpack-node-externals/issues/105
 		...(DEV
 			? [
@@ -56,20 +55,17 @@ module.exports = ({ sessionId }) => ({
 			: []),
 		// @aws-sdk modules are only used in CODE/PROD, so we don't need to
 		// include them in the development bundle
-		// @ts-expect-error - webpack-node-externals types are incorrect:
 		({ request }, callback) => {
 			return process.env.NODE_ENV === 'development' &&
 				request?.startsWith('@aws-sdk')
 				? callback(undefined, `commonjs ${request}`)
 				: callback();
 		},
-		// @ts-expect-error - webpack-node-externals types are incorrect
 		({ request }, callback) => {
 			return request?.endsWith('manifest.json')
 				? callback(undefined, `commonjs ${request}`)
 				: callback();
 		},
-		// @ts-expect-error - webpack-node-externals types are incorrect
 		({ request }, callback) => {
 			return request?.endsWith('manifest.legacy.json')
 				? callback(undefined, `commonjs ${request}`)
